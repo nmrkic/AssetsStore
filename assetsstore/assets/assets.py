2ae5c3a51dd97be6cb0a2a4287b8f4fd0eff7647
Nebojsa Mrkic <mrkic.nebojsa@gmail.com>
2020-10-01 12:54:52 +0200

    -- added rebrandly domain

--- assetsstore/assets/assets.py
+++ assetsstore/assets/assets.py
@@ -72,7 +72,7 @@ class FileAssets(object):
             raise Exception("""There is no asset by name '{}' please set environment variable ASSET_STORE to one of the following:
                 LocalFiles, ServerFiles, S3Files""".format(selected))
         return asset()
-    
+
     def compress(self, file):
         with zipfile.ZipFile(file.replace('.csv', '.zip'), 'w', zipfile.ZIP_DEFLATED) as zipped:
             zipped.write(file, file.split('/')[-1])
@@ -89,25 +89,27 @@ class FileAssets(object):
         else:
             logger.info("Local file does not exist {}".format(local_filename))
         return "Not Deleted"
- 
+
     def shorten_url(self, url):
         try:
             linkRequest = {
-            "destination": url, 
-            "domain": { "fullName": "rebrand.ly" }
-            # , "slashtag": "A_NEW_SLASHTAG"
-            # , "title": "Rebrandly YouTube channel"
+                "destination": url,
+                "domain": {"fullName": os.getenv("REBRAND_DOMAIN", "rebrand.ly")}
+                # , "slashtag": "A_NEW_SLASHTAG"
+                # , "title": "Rebrandly YouTube channel"
             }
 
             requestHeaders = {
-            "Content-type": "application/json",
-            "apikey": os.getenv("REBRAND_KEY"),
-            # "workspace": "YOUR_WORKSPACE_ID"
+                "Content-type": "application/json",
+                "apikey": os.getenv("REBRAND_KEY"),
+                # "workspace": "YOUR_WORKSPACE_ID"
             }
 
-            r = requests.post("https://api.rebrandly.com/v1/links", 
-                data = json.dumps(linkRequest),
-                headers=requestHeaders)
+            r = requests.post(
+                "https://api.rebrandly.com/v1/links",
+                data=json.dumps(linkRequest),
+                headers=requestHeaders
+            )
 
             if (r.status_code == requests.codes.ok):
                 link = r.json()
