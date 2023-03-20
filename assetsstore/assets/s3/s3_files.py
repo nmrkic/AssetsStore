c208c71dd9f97355d7278734b91b57c1345ff4db
Ivan Djuraki <ivandjuraki@protonmail.com>
2023-03-20 15:48:35 +0100

    remove unused upload connection

--- assetsstore/assets/s3/s3_files.py
+++ assetsstore/assets/s3/s3_files.py
@@ -74,13 +74,6 @@ class S3Files(FileAssets):
                 signature_version="s3v4"
             )
         )
-        self.upload_connection = session.client(
-            's3',
-            config=boto3.session.Config(
-                s3={'addressing_style': 'path'},
-                signature_version='s3v4'
-            )
-        )
         self.resource = session.resource('s3')
         super().__init__()
 
@@ -118,7 +111,6 @@ class S3Files(FileAssets):
             for key in bucket.objects.filter(Prefix=folder):
                 if key.meta.data.get('StorageClass', "") == "STANDARD":
                     size += key.size
-
         except Exception as e:
             logger.exception("Cannot get size of the S3 bucket folder. Exception: {}".format(str(e)))
         return size
@@ -158,7 +150,7 @@ class S3Files(FileAssets):
 
         # Set the desired multipart threshold value (5GB)
         try:
-            response = self.upload_connection.generate_presigned_url(
+            response = self.connection.generate_presigned_url(
                 ClientMethod='put_object',
                 Params={
                     'Bucket': self.s3_bucket_name,
